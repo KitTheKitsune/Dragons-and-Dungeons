f5cfb638b20f27057b4a63595145b635a54ed206
KokatsuKitsune <kendricksmith@creighton.edu>
2021-03-26 09:51:32 -0500

    Update Race.java

--- java/Race.java
+++ java/Race.java
@@ -1,20 +1,27 @@
+import AbilityScore.java;
+
+enum RACE {
+  HUMAN,
+  ELF
+};
+
 class Race {
-  private race;
+  private RACE race;
   private ArrayList<AbilityScore> abilityscores;
   
   public Race(){
-    this.race = "human";
+    this.race = RACE.HUMAN;
     this.abilityscores = new ArrayList<AbilityScore>(6);
     for(int i = 0; i<6; i++){
       this.abilityscores.add(new AbiltyScore(1));
     } //END LOOP
   } //END CONSTRUCTOR
   
-  public Race(race){
+  public Race(RACE race){
     this.race = race;
   } //END CONSTRUCTOR 
   
   public getRace(){
     return this.race;
   } 
-}
+};
